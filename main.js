11bc19ced7f941a808450bc1d6f8df5cd80ed0a1
smmmfrd <smmmfrd@gmail.com>
2023-09-07 13:59:04 -0700

    Nav Title Typewrite Effect

--- main.js
+++ main.js
@@ -10,13 +10,35 @@ function handleScroll() {
   const projectPosition = projectElement.getBoundingClientRect();
 
   if (heroPosition.top < window.innerHeight && heroPosition.bottom - 160 >= 0) {
-    navTitle.textContent = "Welcome";
+    startWriting("Welcome");
   } else if (
     projectPosition.top < window.innerHeight &&
     projectPosition.bottom >= 0
   ) {
-    navTitle.textContent = "Projects";
+    startWriting("Projects");
   } else {
-    navTitle.textContent = "Contact";
+    startWriting("Contact");
+  }
+}
+
+var started = false;
+var index = 0;
+var delay = 50;
+
+function startWriting(text) {
+  if (!started && navTitle.textContent !== text) {
+    index = 0;
+    started = true;
+    write(text);
+  }
+}
+
+function write(text) {
+  if (index < text.length) {
+    index++;
+    navTitle.textContent = text.slice(0, index);
+    setTimeout(() => write(text), delay);
+  } else {
+    started = false;
   }
 }

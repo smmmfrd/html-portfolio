e660aa05648a483d676c38d03244bd367e518aa9
smmmfrd <smmmfrd@gmail.com>
2023-09-18 14:28:35 -0700

    Connector and Details Open on Click

--- contact.js
+++ contact.js
@@ -1,4 +1,15 @@
 const contactElements = [...document.querySelectorAll(".contact-item")];
+const contactDetails = document.querySelector("#contact-detail");
+const contactConnector = document.querySelector("#contact-connector");
+
+document.querySelector("#contact-link").addEventListener("mouseleave", () => {
+  detailsOpen = false;
+  contactDetails.style.height = "0";
+  contactDetails.style.marginBottom = "0";
+  contactConnector.style.height = "0";
+});
+
+var detailsOpen = false;
 
 contactElements.forEach((element) => {
   element.addEventListener("click", () => ContactClick(element.id));
@@ -6,4 +17,11 @@ contactElements.forEach((element) => {
 
 function ContactClick(name) {
   console.log(name);
+  if (!detailsOpen) {
+    detailsOpen = true;
+
+    contactDetails.style.height = "7rem";
+    contactDetails.style.marginBottom = "1rem";
+    contactConnector.style.height = "1rem";
+  }
 }

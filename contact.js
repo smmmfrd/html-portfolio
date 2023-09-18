b726acb081b562baf363ea1ac6839e19e5c297f6
smmmfrd <smmmfrd@gmail.com>
2023-09-18 15:02:37 -0700

    Transition Details Connector to Selected

--- contact.js
+++ contact.js
@@ -3,13 +3,14 @@ const contactDetails = document.querySelector("#contact-detail");
 const contactConnector = document.querySelector("#contact-connector");
 
 document.querySelector("#contact-link").addEventListener("mouseleave", () => {
-  detailsOpen = false;
-  contactDetails.style.height = "0";
-  contactDetails.style.marginBottom = "0";
+  detailsOpen = "";
+
+  toggleDetails();
+
   contactConnector.style.height = "0";
 });
 
-var detailsOpen = false;
+var detailsOpen = "";
 
 contactElements.forEach((element) => {
   element.addEventListener("click", () => ContactClick(element.id));
@@ -17,11 +18,26 @@ contactElements.forEach((element) => {
 
 function ContactClick(name) {
   console.log(name);
-  if (!detailsOpen) {
-    detailsOpen = true;
+  if (detailsOpen.length === 0 || detailsOpen !== name) {
+    detailsOpen = name;
 
+    toggleDetails();
+
+    contactConnector.style.height = "1rem";
+    const thing = contactElements
+      .find((element) => element.id === name)
+      .getBoundingClientRect();
+    contactConnector.style.left = `${thing.x - 8}px`;
+    console.log(thing);
+  }
+}
+
+function toggleDetails() {
+  if (detailsOpen.length > 0) {
     contactDetails.style.height = "7rem";
     contactDetails.style.marginBottom = "1rem";
-    contactConnector.style.height = "1rem";
+  } else {
+    contactDetails.style.height = "0";
+    contactDetails.style.marginBottom = "0";
   }
 }

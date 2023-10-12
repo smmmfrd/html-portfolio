b95c947ce2c3da5b8b49337aa8d4a7e123b95979
smmmfrd <smmmfrd@gmail.com>
2023-10-12 12:10:28 -0700

    Modal Showing Image Clicked On

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,13 +1,23 @@
+const imgDialog = document.querySelector("#img-display");
+const imgDisplay = imgDialog.querySelector("img");
+
 function Find() {
   const parents = document.querySelectorAll(".project-img-parent");
+
   const children = Array.from(parents).map((p) => p.querySelector("img"));
+
   parents.forEach((p, index) =>
-    p.addEventListener("click", () => DisplayImage(children[index].src))
+    p.addEventListener("click", () =>
+      DisplayImage(children[index].src, children[index].alt)
+    )
   );
 }
 
-function DisplayImage(img) {
-  console.log(img);
+function DisplayImage(img, altText) {
+  console.log(img, altText);
+  imgDisplay.src = img;
+  imgDisplay.alt = altText;
+  imgDialog.showModal();
 }
 
 export { Find };
